1e15b8c65a3027ad68490a361256071067929037
Carlos03tb <carlostb0318@gmail.com>
2024-11-08 12:17:54 +0000

    terminado

---
+++ src/js/component/JumbotronComponente.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const JumbotronComponente = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="jumbotron mt-5 pt-5">
+                    <div className="card border-0">
+                        <div className="card-body bg-light rounded p-5">
+                            <h1 className="card-title">A Warm Welcome</h1>
+                            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus cum, soluta dicta vitae ab delectus, assumenda quae, quis mollitia sit dolores? Delectus cumque blanditiis esse possimus, voluptate ut magni minima.</p>
+                            <a href="#" className="btn btn-primary">Call to action!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
